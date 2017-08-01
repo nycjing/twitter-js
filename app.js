b158dc9679a21bd431b47eec30c1d0b664889651
Robin Wilson <robin.kath.wilson@gmail.com>
2017-08-01 08:05:51 -0700

    rwilson: added Nunjucks functionality

--- app.js
+++ app.js
@@ -3,10 +3,40 @@ const app = express(); // creates an instance of an express application
 const volleyball = require('volleyball');
 const nunjucks = require('nunjucks');
 
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+// APPS CONFIGURATION
+
 app.use(volleyball);
 
-nunjucks.configure('views', { autoescape: true });
-// nunjucks.render('index.html', { foo: 'bar' });
+/* OLD NUNJUCKS CODE
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});
+*/
+
+// "Nunjucks-Express Combo Meal <3 Merges Express and Nunjucks easily
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+// nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+
+nunjucks.configure('views', { noCache: true }); //Use for learning so no caching, saves time
+
+
+
+
+// SERVER CONFIGURATION
 
 app.use(function (req, res, next) {
     // do your logging here
@@ -16,7 +46,8 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
+// res.send('Hello World!')
 });
 
 app.get('/news', function (req, res) {
