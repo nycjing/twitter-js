af76a28e1765d74d50f49c5f402de1e80385ffc5
Robin Wilson <robin.kath.wilson@gmail.com>
2017-08-01 07:26:39 -0700

    rwilson: started proj

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require( 'express' );
+const app = express(); // creates an instance of an express application
+
+app.use(function (req, res, next) {
+    // do your logging here
+    //res.send("1"); // this is like a return statement
+    console.log("1");
+    next();// call `next`, or else your app will be a black hole — receiving requests but never properly responding
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+app.get('/news', function (req, res) {
+  res.send('Newsssss')
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
